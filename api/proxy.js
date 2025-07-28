cba0d9be624ae31932ceb1d604a45ed46e90f71d
Hamba Penyayang <abd.rokhim@gmail.com>
2025-07-29 00:13:21 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,32 +1,37 @@
 export default async (req, res) => {
   const { url } = req.query;
 
-  if (!url) {
-    return res.status(400).send('Missing "url" query parameter.');
-  }
+  if (!url) return res.status(400).send('Missing "url" query parameter.');
+
+  const headers = { ...req.headers };
+
+  // Jangan kirim headers yang bisa diblok server target
+  delete headers['host'];
+  delete headers['referer'];
+  delete headers['origin'];
 
   try {
     const response = await fetch(url, {
       method: req.method,
-      headers: { ...req.headers, host: new URL(url).host },
+      headers,
       body: ['GET', 'HEAD'].includes(req.method) ? undefined : req.body,
     });
 
     const data = await response.arrayBuffer();
-    const headers = Object.fromEntries(response.headers.entries());
 
     res.status(response.status);
-    Object.entries(headers).forEach(([key, value]) => {
-      res.setHeader(key, value);
-    });
 
-    // Inject CORS headers
+    for (const [key, value] of response.headers.entries()) {
+      res.setHeader(key, value);
+    }
+
+    // Inject CORS Headers
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
     res.setHeader('Access-Control-Allow-Methods', '*');
 
     res.send(Buffer.from(data));
-  } catch (err) {
-    res.status(500).send(err.toString());
+  } catch (e) {
+    res.status(500).send(e.toString());
   }
 };
