d6ec352bd1afd450c7598889f0cba5192bc9a515
Hamba Penyayang <abd.rokhim@gmail.com>
2025-07-28 23:58:54 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -5,23 +5,23 @@ import request from 'request'
 const app = express()
 app.use(cors())
 
-function parseProxyParameters(proxyRequest) {
+function parseProxyParameters(req) {
   const params = {}
-  const urlMatch = proxyRequest.url.match(/(?<=[?&])url=(?<url>.*)$/)
+  const urlMatch = req.url.match(/(?<=[?&])url=(?<url>.*)$/)
   if (urlMatch) {
     params.url = decodeURIComponent(urlMatch.groups.url)
   }
   return params
 }
 
-app.all('/*', (req, res) => {
-  const proxyParams = parseProxyParameters(req)
-  if (!proxyParams.url) {
-    return res.status(400).json({ msg: 'Missing url param' })
+app.all('*', (req, res) => {
+  const { url } = parseProxyParameters(req)
+  if (!url) {
+    return res.status(400).json({ error: 'Missing url parameter' })
   }
-  const target = request(proxyParams.url)
-  req.pipe(target)
-  target.pipe(res)
+  const proxy = request(url)
+  req.pipe(proxy)
+  proxy.pipe(res)
 })
 
 export default app
